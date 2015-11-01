7d5b1cd144b64be8e0f040048beb672075db51df
Roman Zaynetdinov <romanznet@gmail.com>
2015-11-01 16:21:20 +0200

    Update bash setup

--- bash/setup.sh
+++ bash/setup.sh
@@ -4,7 +4,7 @@ set -e
 # Create bash directory
 bash_dir=$HOME/.bash
 if [[ ! -d $bash_dir ]]; then
-  echo "Create $bash_dir dir"
+  echo "Creating $bash_dir dir..."
   mkdir $bash_dir
 fi
 
@@ -13,26 +13,26 @@ fi
 git_aware_dir=$bash_dir/git-aware-prompt
 if [[ ! -d $git_aware_dir ]]; then
   # Clone it if not exists
-  echo "Clone git aware prompt to $bash_dir"
+  echo "Cloning git aware prompt to $bash_dir..."
   (cd $bash_dir && \
     git clone git://github.com/jimeh/git-aware-prompt.git )
 else
   # Update it otherwise
-  echo "Pull git aware prompt changes"
+  echo "Pulling git aware prompt changes..."
   (cd $git_aware_dir && \
     git pull origin master )
 fi
 
 # Copy .bash_profile
 bash_profile_changed=`diff -q bash_profile $HOME/.bash_profile`
-if [[ ! -z $bash_profile_changed ]]; then
-  echo "Copy .bash_profile"
+if [[ ! -f $HOME/.bash_profile || ! -z $bash_profile_changed ]]; then
+  echo "Copying .bash_profile..."
   cp bash_profile $HOME/
 fi
 
 # Validate that .bashrc exists
 if [[ ! -f $HOME/.bashrc ]]; then
-  echo "Create .bashrc"
+  echo "Creating .bashrc..."
   touch $HOME/.bashrc
 fi
 
@@ -40,7 +40,7 @@ fi
 common_str="source \".bash/common.sh\""
 bashrc_sources_common=`grep -Fxq "$(common_str)" $HOME/.bashrc`
 if [[ $bash_profile_changed ]]; then
-  echo "Add sourcing to a common file to bashrc"
+  echo "Adding sourcing to a common file to bashrc..."
   echo "$(common_str)" >> $HOME/.bashrc
 fi
 
